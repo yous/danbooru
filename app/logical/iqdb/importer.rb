10bda46c5b82a2ce1ecef8b5f43810cdafd83541
r888888888 <r888888888@gmail.com>
2014-04-25 17:15:09 -0700

    refactor

--- app/logical/iqdb/importer.rb
+++ app/logical/iqdb/importer.rb
@@ -2,14 +2,13 @@ module Iqdb
   class Importer
     def import!
       Post.find_each do |post|
-        IO.popen("iqdb command #{Danbooru.config.iqdb_file}", "w") do |io|
+        # IO.popen("iqdb command #{Danbooru.config.iqdb_file}", "w+") do |io|
           if File.exists?(post.preview_file_path)
-            puts post.id
             hex = post.id.to_s(16)
-            io.puts "add 0 #{hex}:#{post.preview_file_path}"
+            puts "add 0 #{hex}:#{post.preview_file_path}"
+            # puts "quit"
           end
-          io.puts "quit"
-        end
+        # end
       end
     end
   end
